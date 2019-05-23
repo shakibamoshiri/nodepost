ad5f16f27ca0faac8ee73e7ea616b6afb6569330
Shakbia Moshiri <hi@shakiba.it>
2019-05-23 14:36:54 +0430

    Add first test file

---
+++ test/user.json.test.js
@@ -0,0 +1,25 @@
+const assert = require('assert');
+const user = require( "../database/user.json" );
+
+describe( "Validating user.json file", function(){
+  it( "user.homeTitle should be empty", function(){
+    assert.notEqual( user.homeTitle, "" );
+  });
+
+  if( user.baseURL !== "" ){
+    it( "user.baseURL should start with '/'", function(){
+      assert.equal( user.baseURL.startsWith( "/" ), true );
+    });
+    
+    it( "user.baseURL should NOT contain space", function(){
+      assert.equal( user.baseURL.search( " " ), -1 );
+    });
+
+    it( "user.baseURL should NOT end with '/'", function(){
+      assert.equal( user.baseURL.endsWith( "/" ), false );
+    });
+  }
+
+
+
+})
