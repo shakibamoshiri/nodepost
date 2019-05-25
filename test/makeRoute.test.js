d0c6e2065adc5ead062194e9cdcfa59a99b0ced1
Shakiba Moshiri <hi@shakiba.it>
2019-05-25 13:59:36 +0430

    Add test for makeRoute.js

---
+++ test/makeRoute.test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+
+const utility = require('util');
+const json = require( "../database/route.json" );
+const makeRoute = require( "../utility/makeRoute.js" );
+
+describe( 'Run function: makeRoute( route, list=[], parent="" )', function(){
+    
+    const route = makeRoute( json );
+    it( "return type should be an array", function(){
+        assert.equal( utility.isArray( route ), true );
+    });
+
+    it( "path in route should NOT contain white-spaces", function(){
+        route.forEach(function( path ){
+            assert.equal( path.search( " " ), -1 );
+        })
+    });
+
+})
