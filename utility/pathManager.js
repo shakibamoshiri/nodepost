3d63f248b4327ff62d4b150666c7cb1287052ccc
Shakiba Moshiri <hi@shakiba.it>
2019-05-25 15:16:05 +0430

    Separate createDir() & deleteDir() from manageDir()

--- utility/pathManager.js
+++ utility/pathManager.js
@@ -51,6 +51,121 @@ if( !mainHtmlDir ){
     }
 }
 
+function createDir( notExistDirs, routeDirs, validRequest, homePath, rootPath ){
+    notExistDirs.forEach(function( path, index ){
+        // split each name
+        const names = path.match( /\/?[A-Za-z0-9_.-]+/g );
+        const tmp = path.replace( homePath, "/" );
+        const currentPath = tmp === "/" ? baseURL + tmp : baseURL + tmp + "/";
+        const gitPath = currentPath;
+
+        // current title is the last on on the name with has "/" at the begging
+        // so "/" should be replaced with ""
+        const currentTitle = names.pop().replace( "/", "" );
+
+        // parent title will be the last word in the list
+        const parentTitle = names.join( "" ).match( /[A-Za-z0-9_.-]+$/ );
+
+        // the pack link to the parent will be all names except the root name in the JSON file
+        const parentLink = names.join( "" ).replace( homePath, "/" );
+
+        const absolutePath = rootPath + path;
+        const main =
+`<main>
+	<div class="content-r">
+		<h1>${ currentTitle  }</h1>
+        ${ parentTitle === null && validRequest.map( function( path ){ return `<span>path:</span> <a href="${ baseURL + path }">${ path }</a>`}).join( "<br>" ) || ""  }
+		DD_MM_YYYY
+        <div class="edit-on-github">
+            ${ gitinfo[0] && gitinfo[1] && `<a target="_blank" href="https://github.com/${ gitinfo[0] }/${ gitinfo[1] }/blob/master${ gitPath }main.html">Edit on Github</a>`  || ""  }
+        </div>
+	</div>
+</main>`;
+
+    const header =
+`${ headerFile.replace( '<base href="">', `<base href="${ currentPath }">` ) }
+<div class="header">
+        <div class="content-r">
+          <h1>
+            ${ tmp === "/" ?  homepageTitle : `<a href="${ baseURL }/" >${ homepageTitle }</a>` }
+          </h1>
+          <hr>
+          <h1><a href="${ baseURL + parentLink }">${ parentTitle || "" }</a></h1>
+        </div>
+    </div>`;
+
+
+        try {
+            fs.mkdirSync( absolutePath );
+            fs.symlinkSync( ( parentTitle === null ? "../build/html/footer.html" : "../footer.html" ), absolutePath + "/footer.html" );
+            fs.writeFileSync( absolutePath + "/header.html", header );
+            fs.writeFileSync( absolutePath + "/main.html", main );
+            routeDirs.push( path );
+            log( "\033[1;32mCreate File:\033[0m", "." + path );
+        } catch( exception ){
+            log( exception.message );
+        }
+
+        try {
+            fs.symlinkSync( absolutePath + "/main.html", "./main-html/" + currentTitle );
+        } catch( exception ){
+            if( exception.message.search( "EEXIST" ) === 0 ){
+                try {
+                    const dirs =  absolutePath.split( "/" );
+                    const fileName = dirs.pop();
+                    const parentName = dirs.pop();
+                    if( parentName !== undefined ){
+                        fs.symlinkSync( absolutePath + "/main.html", "./main-html/" + fileName + "-in-" + parentName );
+                    } else {
+                        log( "Not able to create symbolic link for:", fileName );
+                    }
+                } catch( exception ){
+                    log( exception.message );
+                }
+            }
+        }
+    });
+}
+
+function deleteDir( notExistKey ){
+    notExistKey.forEach(function( path ){
+        try {
+            rmdirSyncRec( "." + path );
+            fs.readdirSync( "./main-html" ).forEach(function( file ){
+                if( !fs.existsSync( "./main-html/" + file ) ){
+                    fs.unlinkSync( "./main-html/" + file );
+                }
+            });
+            log( "\033[1;31mDelete File:\033[0m", "." + path );
+        } catch( exception ){
+            log( exception.message );
+        }
+    });
+}
+
+function updateRoutes( routeDirs, routeLink, routePath ){
+    fs.writeFile( "./database/route.dirs", routeDirs.join( "\n" ) , function( error ){
+        if( error ){
+            console.log( error.message );
+        }
+        log( "\033[1;32mUpdate route.dirs ...\033[0m" );
+    });
+
+    fs.writeFile( "./database/route.link", routeLink , function( error ){
+        if( error ){
+            console.log( error.message );
+        }
+        log( "\033[1;32mUpdate route.link ...\033[0m" );
+    });
+
+    fs.writeFile( "./database/route.path", routePath , function( error ){
+        if( error ){
+            console.log( error.message );
+        }
+        log( "\033[1;32mUpdate route.path ...\033[0m" );
+    });
+}
+
 // create and delete directories
 function manageDir( routeJson, routeDirs, rootPath ){
     
@@ -90,79 +205,7 @@ function manageDir( routeJson, routeDirs, rootPath ){
 
         // check for not existing directories
         // this is the first run all files in routeJson should be created
-        notExistDirs.forEach (function( path, index ){
-            // split each name
-            const names = path.match( /\/?[A-Za-z0-9_.-]+/g );
-            const tmp = path.replace( homePath, "/" );
-            const currentPath = tmp === "/" ? baseURL + tmp : baseURL + tmp + "/";
-            const gitPath = currentPath;
-
-            // current title is the last on on the name with has "/" at the begging
-            // so "/" should be replaced with ""
-            const currentTitle = names.pop().replace( "/", "" );
-
-            // parent title will be the last word in the list
-            const parentTitle = names.join( "" ).match( /[A-Za-z0-9_.-]+$/ );
-
-            // the pack link to the parent will be all names except the root name in the JSON file
-            const parentLink = names.join( "" ).replace( homePath, "/" );
-
-            const absolutePath = rootPath + path;
-            const main =
-`<main>
-	<div class="content-r">
-		<h1>${ currentTitle  }</h1>
-        ${ parentTitle === null && validRequest.map( function( path ){ return `<span>path:</span> <a href="${ baseURL + path }">${ path }</a>`}).join( "<br>" ) || ""  }
-		DD_MM_YYYY
-        <div class="edit-on-github">
-            ${ gitinfo[0] && gitinfo[1] && `<a target="_blank" href="https://github.com/${ gitinfo[0] }/${ gitinfo[1] }/blob/master${ gitPath }main.html">Edit on Github</a>`  || ""  }
-        </div>
-	</div>
-</main>`;
-
-            const header =
-`${ headerFile.replace( '<base href="">', `<base href="${ currentPath }">` ) }
-<div class="header">
-        <div class="content-r">
-          <h1>
-            ${ tmp === "/" ?  homepageTitle : `<a href="${ baseURL }/" >${ homepageTitle }</a>` }
-          </h1>
-          <hr>
-          <h1><a href="${ baseURL + parentLink }">${ parentTitle || "" }</a></h1>
-        </div>
-    </div>`;
-
-
-            try {
-			    fs.mkdirSync( absolutePath );
-                fs.symlinkSync( ( parentTitle === null ? "../build/html/footer.html" : "../footer.html" ), absolutePath + "/footer.html" );
-                fs.writeFileSync( absolutePath + "/header.html", header );
-                fs.writeFileSync( absolutePath + "/main.html", main );
-                routeDirs.push( path );
-                log( "\033[1;32mCreate File:\033[0m", "." + path );
-            } catch( exception ){
-                log( exception.message );
-            }
-
-            try {
-                fs.symlinkSync( absolutePath + "/main.html", "./main-html/" + currentTitle );
-            } catch( exception ){
-                if( exception.message.search( "EEXIST" ) === 0 ){
-                    try {
-                        const dirs =  absolutePath.split( "/" );
-                        const fileName = dirs.pop();
-                        const parentName = dirs.pop();
-                        if( parentName !== undefined ){
-                            fs.symlinkSync( absolutePath + "/main.html", "./main-html/" + fileName + "-in-" + parentName );
-                        } else {
-                            log( "Not able to create symbolic link for:", fileName );
-                        }
-                    } catch( exception ){
-                        log( exception.message );
-                    }
-                }
-            }
-        }); // end of forEach of notExistDirs
+        createDir( notExistDirs, routeDirs, validRequest, homePath, rootPath );
 
         if( notExistDirs.length > 0 ){
             routeDirs.sort();
@@ -175,42 +218,11 @@ function manageDir( routeJson, routeDirs, rootPath ){
             return routeJson.indexOf( key ) === -1;
         });
 
-        notExistKey.forEach(function( path ){
-            try {
-                rmdirSyncRec( "." + path );
-                fs.readdirSync( "./main-html" ).forEach(function( file ){
-                    if( !fs.existsSync( "./main-html/" + file ) ){
-                        fs.unlinkSync( "./main-html/" + file );
-                    }
-                });
-                log( "\033[1;31mDelete File:\033[0m", "." + path );
-            } catch( exception ){
-                log( exception.message );
-            }
-        });
+        deleteDir( notExistKey );
 
         routeDirs = routeJson.slice( 0 );
     
-        fs.writeFile( "./database/route.dirs", routeDirs.join( "\n" ) , function( error ){
-            if( error ){
-                console.log( error.message );
-            }
-            log( "\033[1;32mUpdate route.dirs ...\033[0m" );
-        });
-
-        fs.writeFile( "./database/route.link", routeLink , function( error ){
-            if( error ){
-                console.log( error.message );
-            }
-            log( "\033[1;32mUpdate route.link ...\033[0m" );
-        });
-
-        fs.writeFile( "./database/route.path", routePath , function( error ){
-            if( error ){
-                console.log( error.message );
-            }
-            log( "\033[1;32mUpdate route.path ...\033[0m" );
-        });
+        updateRoutes( routeDirs, routeLink, routePath );
 
     } // end of hashes' comparison
 }
