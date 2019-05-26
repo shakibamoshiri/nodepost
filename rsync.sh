22639d703a59abe912968a16e778ec0c91ef92a8
Shakiba Moshiri <hi@shakiba.it>
2019-05-26 14:49:16 +0430

    Remove --delete-excluded option

--- rsync.sh
+++ rsync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rsync --update --delete --delete-excluded --exclude-from=.rsyncignore --port=22 \
+rsync --update --delete --exclude-from=.rsyncignore --port=22 \
     -azv -e "ssh -p 22 -i /home/$USER/.ssh/your-file_rsa"  /your-local-path/ username@your-domain-name.com:/your-remote-path/
