0fe8d6a40837632636569106838fd4059d009f26
Shakiba Moshiri <hi@shakiba.it>
2019-05-28 12:03:20 +0430

    Reformat the content

--- path-manager/getContent.js
+++ path-manager/getContent.js
@@ -1,13 +1,11 @@
 const fs = require( "fs" );
 const updateTime = require( "./updateTime" );
 
-
 function getContent( path, mtime ){
-	// const stat = fs.statSync( path );
     const temp = fs.readFileSync( path + "/main.html", "utf8" );
+	const main = updateTime( temp, mtime );
 	
     const header = fs.readFileSync( path + "/header.html", "utf8" );
-	const main = updateTime( temp, mtime );
 	const footer = fs.readFileSync( path + "/footer.html", "utf8" );
 
 	return header + main + footer;
