cc2b250eb45ea986a550983af50e40863947c222
Shakiba Moshiri <hi@shakiba.it>
2019-05-28 12:47:25 +0430

    Refactor + re-comment

--- route-manager/routeManager.js
+++ route-manager/routeManager.js
@@ -1,30 +1,29 @@
-// route is a JSON
-// list will be filled for each path
-// parent will be the parent of nested directories
+// postsJson is a JSON
+// route will be filled for each path
+// parent will be the parent of nested posts
 // at first there is no parent
-function makeRoute( route, list = [], parent = "" ){
-	for( path in route ){
-		// dir is the directory name
-		// subdir is an Object contains other directories
-		const subRoute = route[ path ];
+function makeRoute( postsJson, route = [], parent = "" ){
+    for( post in postsJson ){
+        // children if there is any
+        const subPosts = postsJson[ post ];
 
-		// parent directory if it has children
-        // also remove extra white-spaces in the route.json file
-		const parentDir = parent + "/" + path.replace( / +/g, "-" );
+        // parent directory if it has children
+        // also remove extra white-spaces in the postsJson.json file
+        const parentDir = parent + "/" + post.replace( / +/g, "-" );
 
-		// store it
-		list.push( parentDir );
+        // store it
+        route.push( parentDir );
 
-		// when we have nested directories then
-		// recursively parse the new Object
-		if( typeof subRoute !== "string" ){
-			// subRoute will be new route
-			// list wil be the first one
-			// parentDir will be the new parent
-			makeRoute( subRoute, list, parentDir )
-		}
-	}
-	return list;
+        // when we have nested posts then
+        // recursively parse the sub-posts
+        if( typeof subPosts !== "string" ){
+            // subPosts will be new postsJson
+            // route wil be continually filled
+            // parentDir will be the new parent
+            makeRoute( subPosts, route, parentDir )
+         }
+    }
+    return route;
 }
 
 module.exports = { makeRoute };
