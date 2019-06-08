560bf857b0e9b9edefaaf6cc7b7d04e515201c3d
Shakiba Moshiri <hi@shakiba.it>
2019-06-08 19:42:55 +0430

    Replace octal with hex

--- path-manager/colorOrganizer.js
+++ path-manager/colorOrganizer.js
@@ -1,16 +1,16 @@
 const log = console.log;
 
 const ANSI = {
-    black:  "\033[1;30m",
-    red:    "\033[1;31m",
-    green:  "\033[1;32m",
-    yellow: "\033[1;33m",
-    blue:   "\033[1;34m",
-    purple: "\033[1;35m",
-    cyan:   "\033[1;36m",
-    white:  "\033[1;37m",
+    black:  "\x1B[1;30m",
+    red:    "\x1B[1;31m",
+    green:  "\x1B[1;32m",
+    yellow: "\x1B[1;33m",
+    blue:   "\x1B[1;34m",
+    purple: "\x1B[1;35m",
+    cyan:   "\x1B[1;36m",
+    white:  "\x1B[1;37m",
 
-    reset:  "\033[0m"
+    reset:  "\x1B[0m"
 };
 
 function colorizeLine( color ){
