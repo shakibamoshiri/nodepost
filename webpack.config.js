438dd478f8d3a29cde615a38cc8fafc3a4354c40
Shakiba Moshiri <hi@shakiba.it>
2019-06-11 17:41:27 +0430

    Filter directories

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const fs = require( "fs" );
 
 const ENTRY_PATH = __dirname + "/main-js/";
-const OUTPUT_PATH = ENTRY_PATH;
+const OUTPUT_PATH = ENTRY_PATH + "/build/"
 
 const mainJsFiles = fs.readdirSync( ENTRY_PATH )
-    .filter( name => name.search( ".bundle.js" ) === -1 )
+    .filter( name => fs.statSync( ENTRY_PATH + "/" + name ).isDirectory() === false )
     .reduce( ( result, name ) => {
         const key = name.slice( 0, name.length - 3 );
         result[ key ] = ENTRY_PATH +  name;
