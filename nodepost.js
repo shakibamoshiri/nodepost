b1e80547ce34606782ad0f8580ea3269cb4a57b8
Shakiba Moshiri <hi@shakiba.it>
2019-06-11 18:43:38 +0430

    Rename "main-js" to "react-js"

--- nodepost.js
+++ nodepost.js
@@ -28,7 +28,7 @@ pm.manageDir( routeJson, routeDirs, rootPath );
 // always servse thise
 nodepost.use( "/build",  express.static( rootPath + "/build" ) );
 nodepost.use( "/vendor",  express.static( rootPath + "/vendor" ) );
-nodepost.use( "/main-js",  express.static( rootPath + "/main-js" ) );
+nodepost.use( "/react-js",  express.static( rootPath + "/react-js" ) );
 
 // cache each post after last modification
 const cache = {};
