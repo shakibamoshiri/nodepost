6f2eb98ae140be0d584b855354a3378646d8e149
Shakiba Moshiri <hi@shakiba.it>
2019-06-12 17:44:52 +0430

    Read main.js files in the blog directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,52 @@
 const fs = require( "fs" );
+const path = require( "path" );
+const log = console.log;
 
-const ENTRY_PATH = __dirname + "/react-js/";
-const OUTPUT_PATH = ENTRY_PATH + "/build/"
+const rootPost = (function(){
+    try {
+        return JSON.parse( fs.readFileSync( __dirname + "/database/posts.json" , "utf8" ) );
+    } catch( exception ){
+        log( exception.message );
+        log( "./database/posts.json is required!" );
+        process.exit( 0 );
+     }
+}());
 
-const mainJsFiles = fs.readdirSync( ENTRY_PATH )
-    .filter( name => fs.statSync( ENTRY_PATH + "/" + name ).isDirectory() === false )
-    .reduce( ( result, name ) => {
-        const key = name.slice( 0, name.length - 3 );
-        result[ key ] = ENTRY_PATH +  name;
-        return result;
+const HOME_DIR = Object.keys( rootPost )[ 0 ];
+const ENTRY_PATH = __dirname + "/" + HOME_DIR;
+const OUTPUT_PATH = __dirname + "/build/react/"
+
+function readDirRec( path, list = [] ){
+   if( fs.existsSync( path ) ){
+       fs.readdirSync( path ).forEach(function( dir ){
+           const newPath = path + "/" + dir;
+           if( fs.statSync( newPath ).isDirectory() ){
+               readDirRec( newPath, list );
+           } else {
+               list.push( newPath );
+           }
+       });
+       list.push( path );
+   }
+
+   return list;
+}
+
+const mainJsFiles =
+readDirRec( ENTRY_PATH ).filter(function( file ){
+    return path.basename( file ) === "main.js";
+}).reduce(function( result, name ){
+    const temp = name.split( "/" );
+    const key = temp.slice( temp.length - 2, -1 );
+    result[ key ] = name;
+    return result;
 }, {});
+
+if( Object.keys( mainJsFiles ).length === 0 ){
+    log( "No main.js file found!" );
+    log( "in:", ENTRY_PATH );
+    process.exit( 0 );
+}
 
 module.exports = {
     mode: 'production',
